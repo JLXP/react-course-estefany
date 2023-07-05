046cab42e2123272430df451b800d36005d8ffdd
JLXP <javier.xool@outlook.com>
2023-07-04 19:39:13 -0600

    Cambios en la api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout'
 import Card from '../../components/Card'
+import { api } from '../../api'
 
 const Home = () => {
+  const [items, setItems] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${api}/products`)
+      const data = await response.json()
+      console.log(data)
+      setItems(data)
+    } catch (error) {
+      console.error(`Oh no, ocurrió un error: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
     <Layout>
-      Home
-      <Card/>
+      {
+        items?.map((item) => {
+           return <Card key={item.id} data={item} />
+        })
+      }
+
     </Layout>
   )
 }
